b3f985a4e1fa4d165201f368a60e4db652f2e59d
Dylan Jacquay <djacquay@Dylans-MacBook-Pro.local>
2015-07-15 15:20:58 -0400

    Initial changes for resume

---
+++ js/resumeBuilder.js
@@ -0,0 +1,68 @@
+var bio = {
+  "name" : "Dylan Jacquay",
+  "role" : " Front End Developer",
+  "contacts" : {
+    "mobile" : "260-494-0462",
+    "email" : "djacquay12@gmail.com",
+    "github" : "dylanjacquay",
+    "twitter" : "@dylanjacquay",
+    "location" : "Fort Wayne, IN"
+  },
+  "welcomeMessage" : "Welcome to my resume!",
+  "skills" : [
+    "awesomeness", "development", "design", "font-end"
+  ],
+  "bioPic" : "images/dylan.jpg"
+}
+
+var work = {
+  "jobs" : [
+    {
+      "employer" : "Level Bits",
+      "position" : "UI/UX Designer",
+      "yearsWorked" : "1 year",
+      "dates" : "May-July",
+      "location" : "Fort Wayne, IN",
+      "description" : "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. "
+    },
+    {
+      "employer" : "Nuvision Properties",
+      "position" : "Freelance",
+      "yearsWorked" : "1 year",
+      "dates" : "May-December",
+      "location" : "Auburn, IN",
+      "description" : "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. "
+    }
+  ]
+}
+
+var education = {
+  "schools" : [
+    {
+      "name" : "Udacity",
+      "degree" : "Front End Development",
+      "dates" : "2015-2016",
+      "location" : "Fort Wayne, IN",
+      "description" : "Went through a series of course and projects, where and learned and implemted HTML, CSS, and JavaScript."
+    }
+  ]
+}
+
+var projects = {
+  "projects" : [
+    {
+      "title" : "My Portfolio",
+      "dates" : "May-Jun",
+      "description" : "Learned HTML and CSS skills in Udacity's Front End Nano Degree course. Used the skills I learned to replicate a page given to me in HTML and CSS, and to also make the page responsive.",
+      "images" : ["images/portfolio.png"]
+    },
+    {
+      "title" : "Resume",
+      "dates" : "Jun-Jul",
+      "description" : "Learned basic JavaScript in Udacity's 'JavaScript for Beginners' course. Used these skills to replicate a resume, and make it my own personal resume.",
+      "images" : ["images/resume.png"]
+    }
+  ]
+}
+
+Sticker.init('.sticker');
